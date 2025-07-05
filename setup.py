10d9fc4692242cda96ddfcd4d7f9ee255873b9a9
Imran Khairuddin <imran_khairuddin@yahoo.com>
2025-07-05 10:14:02 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,26 @@
+from setuptools import setup, find_packages
 
+setup(
+    name='hades',
+    version='1.0.0',
+    description='Enhanced rootkit detection with memory analysis, audit logging, and forensic evidence handling',
+    author='Imran Khairuddin, Falikh Zaifol',
+    author_email='imran.khairuddin@velumlabs.com, falikh.bahri@velumlabs.com',
+    packages=find_packages(),
+    install_requires=[
+        'volatility3',
+        'dissect'
+    ],
+    entry_points={
+        'console_scripts': [
+            'ryoshi-scan=disk.scanner:main',
+            'ryoshi-mem=memory.analyze:main',
+            'ryoshi-log=core.logger:main'
+        ]
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux'
+    ],
+)
